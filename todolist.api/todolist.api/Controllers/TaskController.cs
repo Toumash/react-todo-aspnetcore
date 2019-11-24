e2f15cbf361af415bb1f33b03315b66bf92dd1ae
Tomasz Dłuski <mr.toumash@gmail.com>
2019-11-24 15:22:05 +0100

    adds tasks controller

---
+++ todolist.api/todolist.api/Controllers/TaskController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace todolist.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+
+        private readonly ILogger<TaskController> _logger;
+
+        public TaskController(ILogger<TaskController> logger)
+        {
+            this._logger = logger;
+        }
+
+        public static List<TodoTask> tasks = new List<TodoTask>();
+        // GET: api/Task
+        [HttpGet]
+        public List<TodoTask> Get()
+        {
+            return tasks;
+        }
+
+        // GET: api/Task/5
+        [HttpGet("{id}", Name = "Get")]
+        public TodoTask Get(int id)
+        {
+            return tasks.Where(t => t.Id == id).First();
+        }
+
+        // POST: api/Task
+        [HttpPost]
+        public IActionResult Post([FromBody] [Bind("name")]TodoTask newTask)
+        {
+            tasks.Add(newTask);
+            _logger.LogInformation("Adding task {task}", JsonConvert.SerializeObject(newTask));
+            return NoContent();
+        }
+
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            tasks.RemoveAll(t => t.Id == id);
+            return NoContent();
+        }
+    }
+}
---
+++ todolist.api/todolist.api/Controllers/TodoTask.cs
@@ -0,0 +1,8 @@
+﻿namespace todolist.api.Controllers
+{
+    public class TodoTask
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
